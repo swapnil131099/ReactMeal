541616b65611d35796deaaa1a8bc8d0fcc510a2e
Swapnil <swapnilpal99@gmail.com>
2021-08-22 16:05:26 +0530

    Created sumi's kitchen

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Sumi's Kitchen</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Foods" />
+      </div>
+    </React.Fragment>
+  );
+};
+export default Header;
